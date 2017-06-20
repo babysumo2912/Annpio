7b00d9037ea2562a73aa1ab11fa546e05341ef49
babysumo2912 <boymadeinvietnam@gmail.com>
2017-06-20 21:17:51 +0700

    Version muon

--- application/views/admin/report_hoadonxuat.php
+++ application/views/admin/report_hoadonxuat.php
@@ -1,27 +1,52 @@
 <?php 
 // include'header_admin.php';
+
 ?>
 <?php
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', true);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('Report');
+$pdf->SetTitle('TCPDF Example 006');
 $pdf->SetSubject('TCPDF Tutorial');
 $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' - Lai Thi Lan Anh - 1221050146', PDF_HEADER_STRING);
+
+// set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
 if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 	require_once(dirname(__FILE__).'/lang/eng.php');
 	$pdf->setLanguageArray($l);
 }
+
+// ---------------------------------------------------------
+
+// set font
+$pdf->SetFont('dejavusans', '', 10);
+
+// add a page
 $pdf->AddPage();
+
 if(isset($xuatkho)){
 	foreach ($xuatkho as $key) {};
 	$admin = $this->Admin_models->information($key->id_admin);
@@ -30,23 +55,30 @@ if(isset($xuatkho)){
 		if(isset($chitietxuat)){
 		$i = 1;}
 }}
+$html = '';
 $html = '
+<html>
+<head>
+	<title>Report</title>
+	<meta charset="utf-8">
+</head>
+<body>
 <div>
 	<div style="text-align:center">
-		<h1>Hoa don xuat hang so '.$key->id_xuatkho.'</h1>
+		<h1>Hóa đơn xuất hàng số '.$key->id_xuatkho.'</h1>
 	</div>
 </div>
 <table cellpadding="10px">
 	<tr>
-		<td style="width: 25%">Ngay tao:</td>
+		<td style="width: 25%">Ngày tạo:</td>
 		<td>'.substr($key->ngaythang,0,10).'</td>
 	</tr>
 	<tr>
-		<td>Thoi gian:</td>
+		<td>Thời gian:</td>
 		<td>'.substr($key->ngaythang,11,8).'</td>
 	</tr>
 	<tr>
-		<td>Nguoi tao:</td>
+		<td>Người tạo:</td>
 		<td>'.$ad->name.'</td>
 	</tr>
 </table>
@@ -56,21 +88,21 @@ $html = '
 <div>
 	<table cellpadding="10px">
 		<tr>
-			<td style="width: 25%">Ten khach hang:</td>
+			<td style="width: 25%">Tên khách hàng:</td>
 			<td>'.$key->tenkh.'</td>
 		</tr>
 		<tr>
-			<td>So dien thoai:</td>
+			<td>Số điện thoại:</td>
 			<td>'.$key->sdt.'</td>
 		</tr>
 		<tr>
-			<td>Dia chi:</td>
+			<td>Điạ chỉ:</td>
 			<td>'.$key->diachi.'-'.$key->thanhpho.'</td>
 		</tr>
 	</table>
 </div>
 <div style="text-align: center">
-	<h4>Danh sach san pham</h4>
+	<h4>Danh sách sản phẩm</h4>
 </div>
 <table  cellpadding = "10px" border="1">
 	<tr style="text-align:center">
@@ -78,16 +110,16 @@ $html = '
 		STT
 		</td>
 		<td style="width: 30%">
-		San pham
+		Sản phẩm
 		</td>
 		<td style="width: 20%">
-		Gia ban
+		Giá bán
 		</td>
 		<td style="width: 20%">
-		So luong
+		Số lượng
 		</td>
 		<td style="width: 20%">
-		Thanh tien
+		Thành tiền
 		</td>
 	</tr>
 
@@ -114,18 +146,29 @@ $html .='
 }
 $html .= '
 	<tr style = "text-align: center; background-color: #f5f5f5">
-		<td colspan="3" style="text-align: right">Tong cong:</td>
+		<td colspan="3" style="text-align: right">Tổng cộng:</td>
 		<td>'.number_format($key->soluong).'</td>
 		<td>'.number_format($key->thanhtoan).'</td>
 	</tr>
 </table>
 <div style="text-align: right">
-<p>Nguoi lap phieu</p>
-<p><sup><i>(Ki va ghi ro ho ten)</i></sup></p>
+<p>Người lập phiếu</p>
+<p><sup><i>(Kí và ghi rõ họ tên)</i></sup></p>
 </div>
+</body>
+</html>
 ';
-$pdf->writeHTML($html, true, 0, false, 0);
+$pdf->writeHTML($html, true, false, true, false, '');
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// reset pointer to the last page
 $pdf->lastPage();
+
+// ---------------------------------------------------------
+
+//Close and output PDF document
+// $pdf->Output('example_006.pdf', 'I');
 $pdf->Output('report_xuathang.pdf', 'I');
 ?>
 	
